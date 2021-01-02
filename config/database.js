985b5395c54e89fc3b825464f9d41c2cf9f1bf36
Bill Jenner <bill.jenner@gmail.com>
2021-01-02 14:52:46 -0800

    DB prod connection string

--- config/database.js
+++ config/database.js
@@ -1,5 +1,11 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb:// - CHANGE_ME'}
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    mongoURI:
+      'mongodb+srv://brad123:brad123@devconnector.wgpcu.mongodb.net/vidjot?retryWrites=true&w=majority',
+  };
 } else {
-    module.exports = {mongoURI: 'mongodb+srv://brad123:brad123@devconnector.wgpcu.mongodb.net/vidjot?retryWrites=true&w=majority'}
+  module.exports = {
+    mongoURI:
+      'mongodb+srv://brad123:brad123@devconnector.wgpcu.mongodb.net/vidjot?retryWrites=true&w=majority',
+  };
 }
